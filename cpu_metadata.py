cabb7c69b267f598adf4f6abbd6c67d7f4673847
Michael Tu <michael.tu@yale.edu>
2024-04-20 00:53:08 -0400

    enhanced CPUMetadata to include more ether info

--- cpu_metadata.py
+++ cpu_metadata.py
@@ -1,4 +1,5 @@
 from scapy.fields import BitField, ByteField, ShortField
+from scapy.layers.l2 import DestMACField, SourceMACField
 from scapy.packet import Packet, bind_layers
 from scapy.layers.inet import IP
 from scapy.layers.l2 import Ether, ARP
@@ -7,9 +8,12 @@ TYPE_CPU_METADATA = 0x080a
 
 class CPUMetadata(Packet):
     name = "CPUMetadata"
-    fields_desc = [ ByteField("fromCpu", 0),
+    fields_desc = [ ShortField("srcPort", None),
+                    DestMACField("origEtherDst"),
+                    SourceMACField("origEtherSrc"),
                     ShortField("origEtherType", None),
-                    ShortField("srcPort", None)]
+                    ByteField("fromCpu", 0),
+                ]
 
 bind_layers(Ether, CPUMetadata, type=TYPE_CPU_METADATA)
 bind_layers(CPUMetadata, IP, origEtherType=0x0800)
--- main.py
+++ main.py
@@ -31,7 +31,8 @@ h2, h3 = net.get("h2"), net.get("h3")
 
 print(h2.cmd("arping -c1 10.0.0.3"))
 
-print(h3.cmd("ping -c1 10.0.0.2"))
+print(h3.cmd("ping -c 1 10.0.0.2"))
+print(h3.cmd("ping -c 1 10.0.0.2"))
 
 # These table entries were added by the CPU:
 sw.printTableEntries()
