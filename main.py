7202c269a26e353efba950d02acbceb79f372206
Michael Tu <michael.tu@yale.edu>
2024-04-22 00:14:18 -0400

    added custom topology

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 from p4app import P4Mininet
 
 from controller import MacLearningController
-from my_topo import SingleSwitchTopo
+from my_topo import SingleSwitchTopo, CustomTopo
 
-# Add three hosts. Port 1 (h1) is reserved for the CPU.
-N = 3
+nSwitches, nHosts = 2, 3
+links = [[1,2]]
 
-topo = SingleSwitchTopo(N)
+topo = CustomTopo(nSwitches,nHosts,links)
 net = P4Mininet(program="l2switch.p4", topo=topo, auto_arp=False)
 net.start()
 
-# Add a mcast group for all ports (except for the CPU port)
-bcast_mgid = 1
-sw = net.get("s1")
-sw.addMulticastGroup(mgid=bcast_mgid, ports=range(2, N + 1))
+for i in range(1, nSwitches + 1):
+    swName = 's%d' % i
 
-# Send MAC bcast packets to the bcast multicast group
-sw.insertTableEntry(
-    table_name="MyIngress.fwd_l2",
-    match_fields={"hdr.ethernet.dstAddr": ["ff:ff:ff:ff:ff:ff"]},
-    action_name="MyIngress.set_mgid",
-    action_params={"mgid": bcast_mgid},
-)
+    # Add a mcast group for all ports (except for the CPU port)
+    bcast_mgid = 1
+    sw = net.get(swName)
+    sw.addMulticastGroup(mgid=bcast_mgid, ports=range(2, topo.link_count[swName] + 1))
 
-# Start the MAC learning controller
-cpu = MacLearningController(sw)
-cpu.start()
+    # Send MAC bcast packets to the bcast multicast group
+    sw.insertTableEntry(
+        table_name="MyIngress.fwd_l2",
+        match_fields={"hdr.ethernet.dstAddr": ["ff:ff:ff:ff:ff:ff"]},
+        action_name="MyIngress.set_mgid",
+        action_params={"mgid": bcast_mgid},
+    )
 
-h2, h3 = net.get("h2"), net.get("h3")
+    # Start the MAC learning controller
+    cpu = MacLearningController(sw)
+    cpu.start()
 
-print(h2.cmd("arping -c1 10.0.0.3"))
-print(h2.cmd("arping -c1 10.0.0.3")) # second arping is faster bc response is cached on cpu
+h2, h3 = net.get("s1h2"), net.get("s1h3")
 
-print(h3.cmd("ping -c 5 10.0.0.2"))
-print(h2.cmd("ping -c 5 10.0.0.3"))
+print(h2.cmd("arping -c1 10.0.1.3"))
+print(h2.cmd("arping -c1 10.0.1.3")) # second arping is faster bc response is cached on cpu
+
+print(h3.cmd("ping -c 5 10.0.1.2"))
+print(h2.cmd("ping -c 5 10.0.1.3"))
 
 # These table entries were added by the CPU:
 sw.printTableEntries()
