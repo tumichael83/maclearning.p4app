8580ac67b49e6a2de42344c8623f34ee5f74b152
Michael Tu <michael.tu@yale.edu>
2024-04-22 01:54:55 -0400

    need to add lsus

--- pwospf_handler.py
+++ pwospf_handler.py
@@ -1,13 +1,20 @@
 from scapy.all import Packet, Ether, IP
 from cpu_metadata import CPUMetadata
-from pwospf_packet import PWOSPF_Hdr, PWOSPF_Hello, PWOSPF_Lsu
+from pwospf_packet import PWOSPF_Hdr, PWOSPF_Hello, PWOSPF_Lsu, PWOSPF_LSA
 from datetime import datetime as dt
+
+from collections import namedtuple
+NeighborEntry = namedtuple("NeighborEntry", "")
+
 
 BCAST_MAC = 'ff:ff:ff:ff:ff:ff'
 
 PROTO_PWOSPF = 89
 
 HELLOINT_DFLT = 10
+LSUINT_DFLT = 15
+TTL_DFLT    = 255
+
 ALLOSPFADDR = '224.0.0.5'
 
 class PWOSPFHandler():
@@ -29,6 +36,11 @@ class PWOSPFHandler():
         self.helloint       = HELLOINT_DFLT
         self.last_hello     = dt.now()
 
+        self.lsuint         = LSUINT_DFLT
+        self.last_lsu       = dt.now()
+        self.seq            = 0
+        self.ttl            = TTL_DFLT
+
         # receive PWOSPF broadcasts
         sw.insertTableEntry(
             table_name="MyIngress.local_ip_table",
@@ -39,20 +51,38 @@ class PWOSPFHandler():
 
     def broadcast(self):
         while not self.stop_event.wait(1): # loop in 1 second intervals
-            
+            # send my helloint
             if (dt.now() - self.last_hello).total_seconds() >= self.helloint:
                 self.send_hello()
                 self.last_hello = dt.now()
 
+            # send my lsu
+            if (dt.now() - self.last_lsu).total_seconds() >= self:
+                self.send_lsu()
+                self.last_lsu = dt.now()
+
     def send_hello(self):
         hello = Ether(src=self.mac, dst=BCAST_MAC)
         hello /= CPUMetadata()
-        hello /= IP(src=self.ip, dst=ALLOSPFADDR, ttl=1) # ttl == 1 bc we only broadcast to our neighbors
+        hello /= IP(src=self.ip, dst=ALLOSPFADDR, ttl=2)    # need 2 because its decremented once b4 sending
         hello /= PWOSPF_Hdr(routerID=self.routerID, areaID=self.areaID)
         hello /= PWOSPF_Hello(mask=self.mask, helloint=self.helloint)
+
         self.send(hello)
 
+    def send_lsu(self):
+        print('sending lsu')
+
+        # not really sure about subnet=self.routerID
+        lsalist = [PWOSPF_LSA(subnet=self.routerID,mask=self.mask,routerID=self.routerID)]
+
+
+
+        self.seq += 1
+
     def handle(self, pkt):
+        if not self.is_valid_ospf(pkt): return
+
         if pkt[PWOSPF_Hdr].type == 1:
             self.handle_hello(pkt)
 
@@ -63,6 +93,25 @@ class PWOSPFHandler():
         print('received hello')
 
     def handle_lsu(self, pkt):
-        pass
+        print(self.sw.name+': received lsu')
 
+    def is_valid_ospf(self, pkt):
+        # TODO: Checksum
+        if pkt[PWOSPF_Hdr].version != 2:
+            return False
+        
+        elif pkt[PWOSPF_Hdr].areaID != self.areaID:
+            return False
+        
+        else:
+            return True
+
+
+class PWOSPF_Iface():
+    def __init__(self, mask, helloint):
+
+        self.mask           = mask
+        self.helloint       = helloint
+
+        self.neighbors      = {} # 
 
